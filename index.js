792245629a6f5fea24509baae1e862dd56d792a4
liquidloose <ronaldlussier83@gmail.com>
2021-10-17 04:59:10 +0000

    small changes

--- index.js
+++ index.js
@@ -3,22 +3,19 @@ const container = document.querySelector("#container");
 const content = document.createElement("div");
 content.classList.add("content");
 content.textContent = "This is the glorious text-content!";
-const contentTwo = document.createElement("p"); //red text
+const contentTwo = document.createElement("p");
 contentTwo.textContent = "Hey I'm red!";
 contentTwo.style.color = "red";
-const header = document.createElement('h3');
+const header = document.createElement("h3");
 header.style.color = "green";
 header.textContent = "I'm a green h3!";
-const blackDiv = document.createElement('div');
+const blackDiv = document.createElement("div");
 blackDiv.textContent = "I'm a black border";
-blackDiv.style.border = '5px solid black';
-const blackHeader = document.createElement('h1');
+blackDiv.style.border = "5px solid black";
+const blackHeader = document.createElement("h1");
 blackHeader.textContent = "I'm in a div";
-const meToo = document.createElement('p');
+const meToo = document.createElement("p");
 meToo.textContent = "Me too!";
-
-
-
 
 container.appendChild(content);
 container.appendChild(contentTwo);
@@ -27,17 +24,20 @@ container.appendChild(blackDiv);
 blackDiv.appendChild(blackHeader);
 blackDiv.appendChild(meToo);
 
-const btn = document.querySelector('#btn');
+const btn = document.querySelector("#btn");
 
-btn.addEventListener('click', () => {
+btn.addEventListener("click", () => {
   alert("Hello World");
 });
 
-btn.addEventListener('click', () => {
+btn.addEventListener("click", () => {
   alert("I love lamp");
 });
 
-btn.addEventListener('click', () => {
+btn.addEventListener("click", () => {
   alert("Cats are cool");
 });
 
+btn.addEventListener("click", function (e) {
+  e.target.style.background = "orange";
+});
