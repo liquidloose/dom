3410d1dc18f5c2e6278371231f5caf46a2e90a66
liquidloose <ronaldlussier83@gmail.com>
2021-10-17 04:22:58 +0000

    mini-page with event handler

---
+++ index.js
@@ -0,0 +1,43 @@
+const container = document.querySelector("#container");
+
+const content = document.createElement("div");
+content.classList.add("content");
+content.textContent = "This is the glorious text-content!";
+const contentTwo = document.createElement("p"); //red text
+contentTwo.textContent = "Hey I'm red!";
+contentTwo.style.color = "red";
+const header = document.createElement('h3');
+header.style.color = "green";
+header.textContent = "I'm a green h3!";
+const blackDiv = document.createElement('div');
+blackDiv.textContent = "I'm a black border";
+blackDiv.style.border = '5px solid black';
+const blackHeader = document.createElement('h1');
+blackHeader.textContent = "I'm in a div";
+const meToo = document.createElement('p');
+meToo.textContent = "Me too!";
+
+
+
+
+container.appendChild(content);
+container.appendChild(contentTwo);
+container.appendChild(header);
+container.appendChild(blackDiv);
+blackDiv.appendChild(blackHeader);
+blackDiv.appendChild(meToo);
+
+const btn = document.querySelector('#btn');
+
+btn.addEventListener('click', () => {
+  alert("Hello World");
+});
+
+btn.addEventListener('click', () => {
+  alert("I love lamp");
+});
+
+btn.addEventListener('click', () => {
+  alert("Cats are cool");
+});
+
